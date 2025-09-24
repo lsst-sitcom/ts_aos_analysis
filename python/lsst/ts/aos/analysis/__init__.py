8727b730588c10dc656a0498b9bad68f47bf62c0
gmegh <guillem.megias.homar@gmail.com>
2025-09-24 16:25:08 -0700

    Add MTAOS replica notebook to estimate state from butler zernikes

--- python/lsst/ts/aos/analysis/__init__.py
+++ python/lsst/ts/aos/analysis/__init__.py
@@ -2,6 +2,6 @@ from .blockUtils import *
 from .butlerUtils import *
 from .nightlyAnalyzer import *
 from .nightlyReport import *
+from .stateUtils import *
 from .ofcUtils import *
 from .plotUtils import *
-from .stateUtils import *
